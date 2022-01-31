57a971368f8d9f2d8051e41a721b575e07915857
PhiBrain1628 <98775292+PhiBrain1628@users.noreply.github.com>
2022-01-31 21:05:27 +0100

    Create Pong4_PlayerAnimation.py

---
+++ Pong4_PlayerAnimation.py
@@ -0,0 +1,94 @@
+import pygame as py
+import sys
+
+# General setup
+py.init()
+clock = py.time.Clock()
+
+# Setting up the main window
+screen_width = 858
+screen_height = 525
+screen = py.display.set_mode((screen_width, screen_height))
+py.display.set_caption("Pong Game")
+
+# Define color
+black = (0, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 0)
+
+# Rectangles
+ball = py.Rect(screen_width / 2 - 9, screen_height / 2 - 9, 18, 18)
+player_1 = py.Rect(screen_width - 30, screen_height / 2 - 30, 10, 60)
+player_2 = py.Rect(20 , screen_height / 2 - 30, 10, 60)
+
+# Edge 
+edge_1 = py.Rect(0, 0, 2, 858)
+edge_2 = py.Rect(0, 0, 858, 4)
+edge_3 = py.Rect(screen_width - 2, 0, 4, 858)
+edge_4 = py.Rect(0, screen_height - 2, 858, 4)
+
+# Game Variables
+ball_speed_x = 7
+ball_speed_y = 7
+player1_speed = 0
+player2_speed = 0
+
+def player_animation():
+    global player1_speed, player2_speed
+
+    player_1.y += player1_speed
+    player_2.y += player2_speed
+
+    if player_1.top <= 0:
+        player_1.top = 0
+    if player_1.bottom >= screen_height:
+        player_1.bottom = screen_height
+
+    if player_2.top <= 0:
+        player_2.top = 0
+    if player_2.bottom >= screen_height:
+        player_2.bottom = screen_height
+
+# --- Main Program Loop ---
+while True:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            sys.exit()
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_DOWN:
+                player1_speed += 7
+            if event.key == py.K_UP:
+                player1_speed -= 7
+            if event.key == py.K_w:
+                player2_speed -= 7
+            if event.key == py.K_s:
+                player2_speed += 7
+        if event.type == py.KEYUP:
+            if event.key == py.K_DOWN:
+                player1_speed -= 7
+            if event.key == py.K_UP:
+                player1_speed += 7
+            if event.key == py.K_w:
+                player2_speed += 7
+            if event.key == py.K_s:
+                player2_speed -= 7
+    
+    # Game Logic
+    ball_animation()
+    player_animation()
+    
+    # Visuals
+    screen.fill(black)
+    py.draw.rect(screen, white, edge_1)
+    py.draw.rect(screen, white, edge_2)
+    py.draw.rect(screen, white, edge_3)
+    py.draw.rect(screen, white, edge_4)
+    py.draw.ellipse(screen, white, ball)
+    py.draw.rect(screen, white, player_1)
+    py.draw.rect(screen, white, player_2)
+    py.draw.aaline(screen, white, (screen_width / 2, 0), (screen_width / 2, screen_height))
+
+    # Updating the window
+    py.display.flip()
+    clock.tick(60)
